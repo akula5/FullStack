893ccdbd845b838e792560eee00400e592b91ff8
akula5 <niharikaak05@gmail.com>
2023-06-02 12:03:31 -0500

    commit

---
+++ getname/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String Name="Niharika";
+    public String getName() {
+        return Name;
+    }
+
+    public String setName(String n) {
+        Name = n;
+        return Name;
+    }
+}
