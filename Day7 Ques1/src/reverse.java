2ec781e6462ddc1f22f602a112eb2b6f9ebecc3d
akula5 <134973711+akula5@users.noreply.github.com>
2023-06-07 15:01:00 -0500

    Create reverse.java

---
+++ Day7 Ques1/src/reverse.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a[] = {3,99,40,100,66,38,19,2};
+        int i;
+        for(i=(a.length)-1;i>=0;i--)
+        {
+            System.out.print(a[i]+"\t");
+        }
+
+
+    }
+}
